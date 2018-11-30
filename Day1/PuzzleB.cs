514a8303b8fb937811bc8a2d8620774c9f4dbd9a
Marius Fekete <Marius.Fekete@hyland.com>
2018-11-30 09:54:29 -0500

    initial commit

---
+++ Day1/PuzzleB.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+    public class PuzzleB
+    {
+    }
+}
---
+++ Day2/PuzzleB.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+    public class PuzzleB
+    {
+    }
+}
